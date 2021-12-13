306f0dc3e39462d2d98566d33161a5cac3eea715
Imtiaz Adar <75721079+imtiazaadar@users.noreply.github.com>
2021-12-13 22:53:20 +0600

    Youtube Automation - Imtiaz Adar

---
+++ YoutubeAutomation.py
@@ -0,0 +1,18 @@
+# Youtube Automation
+# Imtiaz Adar
+
+import pyautogui as auto
+from time import sleep
+sleep(3)
+auto.moveTo(254, 1058)
+auto.click()
+sleep(2)
+auto.write('Crome')
+sleep(2)
+auto.press('enter')
+sleep(4)
+auto.moveTo(740, 559)
+auto.click()
+sleep(2)
+auto.write('www.youtube.com')
+auto.press('enter')
